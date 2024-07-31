88a2095435e62d8f3b81f62226fff0116e190037
Victoria <vickydewinne@gmail.com>
2024-07-30 23:32:57 -0300

    actuslizacion13

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
-// Inicialización del primer swiper
-var swiper1 = new Swiper(".mySwiper-1", {
-    slidesPerView: 1,
+var swiper = new swiper(".mySwiper-1", {
+    slidesPerView:1,
     spaceBetween: 30,
-    loop: true,
+    loop:true,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+        el:".swiper-pagination",
+        clickable: true,        
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
     }
-});
+}); 
 
-// Inicialización del segundo swiper
-var swiper2 = new Swiper(".mySwiper-2", {
-    slidesPerView: 3,
+var swiper = new swiper(".mySwiper-2", {
+    slidesPerView:3,
     spaceBetween: 20,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    loop:true,
+    loopFillGroupWithBlank:true,
+    
+    nagivation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
     },
-    breakpoints: {
+    breakpoints : {
         0: {
-            slidesPerView: 1,
+            slidesPerView:1,
         },
         520: {
-            slidesPerView: 2,
+            slidesPerView:2,
         },
-        950: {
-            slidesPerView: 3,
+        950: { 
+            slidesPerView:3,
         }
     }
 });
 
-// Manejo de eventos para los inputs de tabulación
 let tabInputs = document.querySelectorAll(".tabInput");
-tabInputs.forEach(function(input) {
-    input.addEventListener('change', function() {
-        let id = input.getAttribute('aria-valuemax'); // Cambiado a getAttribute para obtener el valor correcto
-        let thisSwiper = document.getElementById('swiper' + id);
 
-        if (thisSwiper && thisSwiper.swiper) { // Verificación adicional para evitar errores
-            thisSwiper.swiper.update();
-        }
-    });
+tabInputs.forEach(function(input) {
+
+    input.addEventListener('change', function() {
+        let id = input.ariaValueMax;
+        let thisSwiper = document.getElementById('swiper' + id);
+        thisSwiper.swiper.update();
+    })
 });
